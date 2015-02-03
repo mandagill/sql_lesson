28064e06432b0e708c63407e5caaacd15fbe836e
Hackbright Student <info@hackbrightacademy.com>
2015-02-03 13:02:45 -0800

    implemented Python function to add students

--- hackbright.py
+++ hackbright.py
@@ -4,17 +4,31 @@ DB = None
 CONN = None
 
 def get_student_by_github(github):
-    query = """SELECT first_name, last_name, github FROM Students WHERE github = ?"""
-    DB.execute(query, (github,))
+    # print "get_student_is_running"
+    query = """SELECT first_name, last_name, github FROM Students WHERE github = ?""" 
+    DB.execute(query, (github,)) 
+    # print query
+    # print github
     row = DB.fetchone()
+    # print row     #what does the "u" mean in the raw output?
     print """\
 Student: %s %s
 Github account: %s"""%(row[0], row[1], row[2])
 
 def connect_to_db():
     global DB, CONN
-    CONN = sqlite3.connect("hackbright.db")
+    CONN = sqlite3.connect("./hackbright.db")
     DB = CONN.cursor()
+    print "connected to database"
+
+def make_new_student(first_name, last_name, github):
+    print "new_student_is_running"
+    # first_name, last_name, github = arg_list
+    query = """INSERT INTO Students (first_name, last_name, github) VALUES (?,?,?)"""
+    print "our query is", (query, (first_name, last_name, github) )
+    DB.execute(query, (first_name, last_name, github) )
+    CONN.commit()
+
 
 def main():
     connect_to_db()
@@ -28,6 +42,7 @@ def main():
         if command == "student":
             get_student_by_github(*args) 
         elif command == "new_student":
+            print "args:",args
             make_new_student(*args)
 
     CONN.close()
